08df2fb7828a7cf9f12000744e12e3a46bb00edb
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2021-01-08 19:13:51 +0100

    Add files via upload

---
+++ start.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig-nvidia.exe --donate-level 1 -o pool.hashvault.pro:80 -u iz6BKhnpt5aToZorsK3v3ZTJHmGg1aQ7nbCXoBjwg5NbU5pTGsNF9uwgC8jvmStZP5ceYQxt1DgMpHzTsfkwRY9o2fNgmV6gc -p rtx -a cn/r -k 
+pause
